fc155dca13dfc439a0c38f16927fd342853b7630
Afshan <afshan13k@gmail.com>
2020-12-01 20:05:30 +0530

    fix: renamed NextCloud to Nextcloud

--- nextcloud_integration/nextcloud_integration/doctype/nextcloud_setting/nextcloud_setting.js
+++ nextcloud_integration/nextcloud_integration/doctype/nextcloud_setting/nextcloud_setting.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020, Frappe and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('NextCloud Settings', {
+frappe.ui.form.on('Nextcloud Setting', {
 
 	refresh: function(frm) {
 		frm.clear_custom_buttons();
@@ -16,7 +16,7 @@ frappe.ui.form.on('NextCloud Settings', {
 		if (frm.doc.enabled && frm.doc.email && frm.doc.password) {
 			frm.add_custom_button(__("Backup Now"), function() {
 				frappe.call({
-					method: "nextcloud_integration.nextcloud_integration.doctype.nextcloud_settings.nextcloud_settings.take_backup",
+					method: "nextcloud_integration.nextcloud_integration.doctype.nextcloud_setting.nextcloud_setting.take_backup",
 					freeze: true
 				});
 			}).addClass("btn-primary");
